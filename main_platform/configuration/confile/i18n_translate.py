283c559a2af86997ae7b45402612270226d8980b
Kaki In <91763754+Kaki-In@users.noreply.github.com>
2023-07-29 20:23:56 +0200

    Typing error

--- main_platform/configuration/confile/i18n_translate.py
+++ main_platform/configuration/confile/i18n_translate.py
@@ -14,7 +14,7 @@ class I18nConfigurationFile(ConfigurationFile):
         configuration = self.configuration()
         language = configuration[ "language" ]
         default = configuration[ "default" ]
-        i18n = _i18n.I18nTranslator(language[ 0 ] + "_" + language[ 1 ], default[ 0 ] + "_" + default[ 1 ])
+        i18n = _i18n.I18NTranslator(language[ 0 ] + "_" + language[ 1 ], default[ 0 ] + "_" + default[ 1 ])
         return i18n
     
 
