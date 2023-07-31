2303cf6421e9f678440176a077af7c172661c2c8
Kaki In <91763754+Kaki-In@users.noreply.github.com>
2023-07-31 11:51:05 +0200

    Nothing works T-T

--- main_platform/world/users/user/mail/__init__.py
+++ main_platform/world/users/user/mail/__init__.py
@@ -57,7 +57,7 @@ class MailAddress():
         
         smtp = platform.configuration().mailConfiguration.getSMTP()
         try:
-            smtp.sendmail(sender, self._address, html.encode(encoding="utf-8"))
+            smtp.sendmail(sender, self._address, message.as_bytes(encoding="utf-8") )
         except Exception as exc:
             platform.verbosePolicy().log(platform.i18n().translate("USER_EMAIL_SEND_FAILED", type=type(exc).__name__, error=str(exc)), infolevel = LEVEL_ERROR)
             result = False
